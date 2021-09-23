c834e63b64def61a167587b577bdf0d14ea1e4f4
Nikolay Karadzhov <nikolayk@starsgroup.com>
2021-09-23 23:41:41 +0300

    initial implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import useInternetSpeed from './useInternetSpeed'
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cfg = {
+  timesToTest: 3,
+  interval: 3000,
+  url: 'https://upload.wikimedia.org/wikipedia/commons/e/e6/1kb.png',
+  size: 1000,
 }
 
-export default App;
+const getQuality = speed => {
+  if (speed > 130) return 1080
+  if (speed > 80) return 720
+  return 480
+}
+
+function App() {
+  const getSpeed = useInternetSpeed(cfg)
+  const [speed, setSpeed] = useState('n/a')
+  const [quality, setQuality] = useState('n/a')
+  return (
+    <div style={{ padding: '1em' }}>
+      <p>INTERNET SPEED: {speed} kbps</p>
+      <p> VIDEO QUALITY: {quality} </p>
+      <button
+        onClick={() => {
+          const speed = getSpeed()
+          console.log(speed)
+          setSpeed(speed)
+          setQuality(getQuality(speed))
+        }}
+      >
+        GET SPEED
+      </button>
+    </div>
+  )
+}
+
+export default App
